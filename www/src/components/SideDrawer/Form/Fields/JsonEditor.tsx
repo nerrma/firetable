f795338806b6f81860b56e7ad8d4e5532c901a06
Sina Alsharif <sina.alsharif@gmail.com>
2020-03-31 11:38:13 +1100

    Set edited data in form

--- www/src/components/SideDrawer/Form/Fields/JsonEditor.tsx
+++ www/src/components/SideDrawer/Form/Fields/JsonEditor.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import { FieldProps } from "formik";
 //import ReactJson from "react-json-view";
 import Table from "@material-ui/core/Table";
@@ -8,8 +8,29 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import Tooltip from '@material-ui/core/Tooltip';
+import Fab from '@material-ui/core/Fab';
+import EditIcon from "@material-ui/icons/Edit";
+import DoneIcon from '@material-ui/icons/Done';
 
-import { makeStyles, createStyles, useTheme } from "@material-ui/core";
+import {useFiretableContext} from 'contexts/firetableContext';
+
+import { makeStyles, createStyles, useTheme, withStyles } from "@material-ui/core";
+//import { TextField } from "formik-material-ui";
+import TextField from '@material-ui/core/TextField';
+import { valueCellContentRenderer } from "react-data-grid";
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: "#e22729",
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+//const { updateCell } = useFiretableContext();
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -27,7 +48,7 @@ const useStyles = makeStyles(theme =>
       overflowX: "auto",
     },
     table: {
-      minWidth: 650,
+      minWidth: 250,
     },
   })
 );
@@ -42,13 +63,79 @@ const isValidJson = (val: any) => {
   return true;
 };
 
+const CustomRow = ({val, field, form, shareVal, index}) => {
+        
+        const [isEditing, setisEditing] = useState(false);
+        const [shares, setShares] = useState(shareVal);
+
+        const handleEdit = () => {
+          setisEditing(true);
+        };
+
+        const handleChange = (e, name) => {
+          setShares(e.target.value);
+        };
+
+        const finalEdit = (name) => {
+          setisEditing(false);
+          if(!isNaN(shares)){
+            //console.log("Input: " + e.target.value + " @ " + name);
+            field.value[name].shareTotal = shares;
+            console.log("Field: " + JSON.stringify(field));
+          } else{
+            setShares(shareVal);
+          }
+        }
+        console.log("value: " + JSON.stringify(val));
+        let retailer;
+
+        switch(val[0]){
+          case "F63YLxlqsllN3Ry2tnZl":
+            retailer = "HFG (HelloFresh)";
+            break;
+          case "dvJFrauoNHljAE4Gs6C4":
+            retailer = "AGL CommFac";
+            break;
+          case "knKgDArbnBHAe8t0Hgyv":
+            retailer = "AVG (Australian Vintage)";
+            break;
+        }
+
+        return (
+          <TableRow>
+            <TableCell component="th" scope="row">
+              {retailer}
+            </TableCell>
+
+            {isEditing ? (
+              <TableCell align="right">
+              <TextField onChange={e => handleChange(e, val[0])} label="Shares" value={shares}/>
+            </TableCell> ) : (
+              <TableCell align="right">
+                {shares}
+              </TableCell>
+            )}
+              <TableCell>
+                    {isEditing ? (
+                      <Tooltip title="Finish">
+                        <DoneIcon onClick={() => finalEdit(val[0])}/>
+                      </Tooltip>
+                    ) : (
+                      <Tooltip title="Edit">
+                        <EditIcon onClick={e => handleEdit()} />
+                       </Tooltip> 
+                       )}
+              </TableCell>
+          </TableRow>
+        );
+   }
 export default function JsonEditor({ form, field }: FieldProps) {
   const classes = useStyles();
   const theme = useTheme();
-
-  const handleEdit = edit => {
-    form.setFieldValue(field.name, edit.updated_src);
-  };
+  
+  // const handleEdit = edit => {
+  //   form.setFieldValue(field.name, edit.updated_src);
+  // };
 
   return (
     <div className={classes.root}>
@@ -56,34 +143,17 @@ export default function JsonEditor({ form, field }: FieldProps) {
         <Table className={classes.table} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Company</TableCell>
-              <TableCell align="right">Retailer</TableCell>
-              <TableCell align="right">Total Shares</TableCell>
-              <TableCell align="right">First Purchase</TableCell>
+              <StyledTableCell>Company</StyledTableCell>
+              <StyledTableCell align="right">Total Shares</StyledTableCell>
+              <StyledTableCell align="right"></StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {Object.entries(field.value).map((val: any, index) => {
-              return (
-                <TableRow key={index}>
-                  <TableCell component="th" scope="row">
-                    {val.name}
-                  </TableCell>
-                  <TableCell align="right">{val.shareTotal}</TableCell>
-                  <TableCell align="right">
-                    {val.firstPurchaseTimestamp}
-                  </TableCell>
-                </TableRow>
-              );
+              return(
+              <CustomRow field={field} form={form} val={val} index={index} shareVal={val[1].shareTotal / Math.pow(10, 8)}/>
+              )
             })}
-            <TableRow>
-              <TableCell component="th" scope="row">
-                Testing 123
-              </TableCell>
-              <TableCell align="right">AYP</TableCell>
-              <TableCell align="right">213</TableCell>
-              <TableCell align="right">12:30</TableCell>
-            </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
