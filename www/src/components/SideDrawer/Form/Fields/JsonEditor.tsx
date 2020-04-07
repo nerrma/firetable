f29bb2f3ebecf1c219da24b743ce02c78e7884b8
Sina <sina.alsharif@gmail.com>
2020-04-07 11:49:41 +1000

    Update JSON table and fix NULL Editor

--- www/src/components/SideDrawer/Form/Fields/JsonEditor.tsx
+++ www/src/components/SideDrawer/Form/Fields/JsonEditor.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from "react";
+import React, { useState } from "react";
 import { FieldProps } from "formik";
 //import ReactJson from "react-json-view";
 import Table from "@material-ui/core/Table";
@@ -8,17 +8,13 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import Tooltip from '@material-ui/core/Tooltip';
-import Fab from '@material-ui/core/Fab';
+import Tooltip from "@material-ui/core/Tooltip";
+//import Fab from '@material-ui/core/Fab';
 import EditIcon from "@material-ui/icons/Edit";
-import DoneIcon from '@material-ui/icons/Done';
-
-import {useFiretableContext} from 'contexts/firetableContext';
-
-import { makeStyles, createStyles, useTheme, withStyles } from "@material-ui/core";
+import DoneIcon from "@material-ui/icons/Done";
+import { makeStyles, createStyles, withStyles } from "@material-ui/core";
 //import { TextField } from "formik-material-ui";
-import TextField from '@material-ui/core/TextField';
-import { valueCellContentRenderer } from "react-data-grid";
+import TextField from "@material-ui/core/TextField";
 
 const StyledTableCell = withStyles(theme => ({
   head: {
@@ -53,87 +49,80 @@ const useStyles = makeStyles(theme =>
   })
 );
 
-const isValidJson = (val: any) => {
-  try {
-    if (typeof val === "string") JSON.parse(val);
-    else JSON.stringify(val);
-  } catch (error) {
-    return false;
+const CustomRow = ({ val, field, form, shareVal }) => {
+  const [isEditing, setisEditing] = useState(false);
+  const [shares, setShares] = useState(shareVal);
+
+  const handleEdit = () => {
+    setisEditing(true);
+  };
+
+  const handleChange = (e, name) => {
+    setShares(e.target.value);
+  };
+
+  const finalEdit = name => {
+    setisEditing(false);
+    if (!isNaN(shares)) {
+      let temp = { ...field.value };
+      temp[name].shareTotal = shares * Math.pow(10, 8);
+      form.setFieldValue(field.name, JSON.parse(JSON.stringify(temp)));
+
+      console.trace();
+      console.log("Field: ", field);
+      console.log("Form: ", form);
+    } else {
+      setShares(shareVal);
+    }
+  };
+  console.log("value: " + JSON.stringify(val));
+  let retailer;
+
+  switch (val[0]) {
+    case "F63YLxlqsllN3Ry2tnZl":
+      retailer = "HFG (HelloFresh)";
+      break;
+    case "dvJFrauoNHljAE4Gs6C4":
+      retailer = "AGL CommFac";
+      break;
+    case "knKgDArbnBHAe8t0Hgyv":
+      retailer = "AVG (Australian Vintage)";
+      break;
   }
-  return true;
+
+  return (
+    <TableRow>
+      <TableCell component="th" scope="row">
+        {retailer}
+      </TableCell>
+
+      {isEditing ? (
+        <TableCell align="right">
+          <TextField
+            onChange={e => handleChange(e, val[0])}
+            label="Shares"
+            value={shares}
+          />
+        </TableCell>
+      ) : (
+        <TableCell align="right">{shares}</TableCell>
+      )}
+      <TableCell>
+        {isEditing ? (
+          <Tooltip title="Finish">
+            <DoneIcon onClick={() => finalEdit(val[0])} />
+          </Tooltip>
+        ) : (
+          <Tooltip title="Edit">
+            <EditIcon onClick={e => handleEdit()} />
+          </Tooltip>
+        )}
+      </TableCell>
+    </TableRow>
+  );
 };
-
-const CustomRow = ({val, field, form, shareVal, index}) => {
-        
-        const [isEditing, setisEditing] = useState(false);
-        const [shares, setShares] = useState(shareVal);
-
-        const handleEdit = () => {
-          setisEditing(true);
-        };
-
-        const handleChange = (e, name) => {
-          setShares(e.target.value);
-        };
-
-        const finalEdit = (name) => {
-          setisEditing(false);
-          if(!isNaN(shares)){
-            //console.log("Input: " + e.target.value + " @ " + name);
-            field.value[name].shareTotal = shares;
-            console.log("Field: " + JSON.stringify(field));
-          } else{
-            setShares(shareVal);
-          }
-        }
-        console.log("value: " + JSON.stringify(val));
-        let retailer;
-
-        switch(val[0]){
-          case "F63YLxlqsllN3Ry2tnZl":
-            retailer = "HFG (HelloFresh)";
-            break;
-          case "dvJFrauoNHljAE4Gs6C4":
-            retailer = "AGL CommFac";
-            break;
-          case "knKgDArbnBHAe8t0Hgyv":
-            retailer = "AVG (Australian Vintage)";
-            break;
-        }
-
-        return (
-          <TableRow>
-            <TableCell component="th" scope="row">
-              {retailer}
-            </TableCell>
-
-            {isEditing ? (
-              <TableCell align="right">
-              <TextField onChange={e => handleChange(e, val[0])} label="Shares" value={shares}/>
-            </TableCell> ) : (
-              <TableCell align="right">
-                {shares}
-              </TableCell>
-            )}
-              <TableCell>
-                    {isEditing ? (
-                      <Tooltip title="Finish">
-                        <DoneIcon onClick={() => finalEdit(val[0])}/>
-                      </Tooltip>
-                    ) : (
-                      <Tooltip title="Edit">
-                        <EditIcon onClick={e => handleEdit()} />
-                       </Tooltip> 
-                       )}
-              </TableCell>
-          </TableRow>
-        );
-   }
 export default function JsonEditor({ form, field }: FieldProps) {
   const classes = useStyles();
-  const theme = useTheme();
-  
-  // const handleEdit = edit => {
   //   form.setFieldValue(field.name, edit.updated_src);
   // };
 
@@ -150,9 +139,15 @@ export default function JsonEditor({ form, field }: FieldProps) {
           </TableHead>
           <TableBody>
             {Object.entries(field.value).map((val: any, index) => {
-              return(
-              <CustomRow field={field} form={form} val={val} index={index} shareVal={val[1].shareTotal / Math.pow(10, 8)}/>
-              )
+              return (
+                <CustomRow
+                  field={field}
+                  form={form}
+                  val={val}
+                  key={index}
+                  shareVal={val[1].shareTotal / Math.pow(10, 8)}
+                />
+              );
             })}
           </TableBody>
         </Table>
