b6b3559b0c8cd7667f5b05537c85d45452d23b50
Sina <sina.alsharif@gmail.com>
2020-04-21 12:55:34 +1000

    Added second JSON editor for punchcards

--- www/src/components/SideDrawer/Form/Fields/JsonEditor.tsx
+++ www/src/components/SideDrawer/Form/Fields/JsonEditor.tsx
@@ -12,6 +12,7 @@ import Tooltip from "@material-ui/core/Tooltip";
 //import Fab from '@material-ui/core/Fab';
 import EditIcon from "@material-ui/icons/Edit";
 import DoneIcon from "@material-ui/icons/Done";
+import Checkbox from "@material-ui/core/Checkbox";
 import {
   makeStyles,
   createStyles,
@@ -64,7 +65,7 @@ const isValidJson = (val: any) => {
   return true;
 };
 
-const CustomRow = ({ val, field, form, shareVal }) => {
+const HoldingCustomRow = ({ val, field, form, shareVal }) => {
   const [isEditing, setisEditing] = useState(false);
   const [shares, setShares] = useState(shareVal);
   const [save, setSave] = useState(false);
@@ -79,7 +80,7 @@ const CustomRow = ({ val, field, form, shareVal }) => {
     setShares(e.target.value);
   };
 
-  const finalEdit = retailerID => {
+  const finalEdit = (retailerID: string) => {
     setisEditing(false);
     if (!isNaN(shares)) {
       //newval[retailerName].shareTotal = shares * Math.pow(10,8);
@@ -182,7 +183,7 @@ const HoldingsEditor = ({ form, field }) => {
         <TableBody>
           {Object.entries(field.value).map((val: any, index) => {
             return (
-              <CustomRow
+              <HoldingCustomRow
                 field={field}
                 form={form}
                 val={val}
@@ -191,6 +192,134 @@ const HoldingsEditor = ({ form, field }) => {
               />
             );
           })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+const PunchCustomRow = ({ field, form }) => {
+  const [isEditing, setisEditing] = useState(false);
+  const [active, setActive] = useState(field.value.active);
+  const [count, setCount] = useState(field.value.count);
+  const [details, setDetails] = useState(field.value.details);
+  const [save, setSave] = useState(false);
+
+  const handleEdit = () => {
+    setisEditing(true);
+  };
+
+  const handleChange = e => {
+    var name = e.target.getAttribute("name");
+    setSave(false);
+    switch (name) {
+      case "active":
+        setActive(e.target.checked);
+        break;
+      case "count":
+        setCount(e.target.value);
+        break;
+      case "details":
+        setDetails(e.target.value);
+        break;
+    }
+  };
+
+  const finalEdit = () => {
+    setisEditing(false);
+    let newval = {
+      active: active ? active : false,
+      count: count ? parseInt(count, 10) : 0,
+      details: details ? details : "",
+    };
+
+    form.setFieldValue(field.name, newval);
+
+    console.trace();
+    console.log("Field: ", field);
+    console.log("Form: ", form);
+    setSave(true);
+  };
+  console.log("value: ", field.value);
+
+  return (
+    <TableRow>
+      {save === true && <Autosave values={form.values} errors={form.errors} />}
+      <TableCell align="right">
+        {isEditing ? (
+          <Checkbox
+            onChange={e => handleChange(e)}
+            checked={active}
+            name="active"
+          />
+        ) : (
+          <Checkbox
+            onChange={e => handleChange(e)}
+            checked={active}
+            name="active"
+            disabled
+          />
+        )}
+      </TableCell>
+
+      {isEditing ? (
+        <TableCell align="right">
+          <TextField
+            onChange={e => handleChange(e)}
+            name="count"
+            label="Count"
+            type="number"
+            value={count}
+          />
+        </TableCell>
+      ) : (
+        <TableCell align="right">{count}</TableCell>
+      )}
+
+      {isEditing ? (
+        <TableCell align="right">
+          <TextField
+            onChange={e => handleChange(e)}
+            label="Details"
+            name="details"
+            value={details}
+            inputProps={{ style: { fontSize: 14 } }}
+            fullWidth={true}
+          />
+        </TableCell>
+      ) : (
+        <TableCell align="right">{details}</TableCell>
+      )}
+      <TableCell>
+        {isEditing ? (
+          <Tooltip title="Finish">
+            <DoneIcon onClick={() => finalEdit()} />
+          </Tooltip>
+        ) : (
+          <Tooltip title="Edit">
+            <EditIcon onClick={() => handleEdit()} />
+          </Tooltip>
+        )}
+      </TableCell>
+    </TableRow>
+  );
+};
+
+const PunchCardEditor = ({ form, field }) => {
+  const classes = useStyles();
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Active</StyledTableCell>
+            <StyledTableCell align="right">Count</StyledTableCell>
+            <StyledTableCell align="right">Details</StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <PunchCustomRow field={field} form={form} />
         </TableBody>
       </Table>
     </TableContainer>
@@ -207,40 +336,47 @@ export default function JsonEditor({ form, field }: FieldProps) {
 
   return (
     <div className={classes.root}>
-      {field.name === "holdings" ? (
-        <HoldingsEditor form={form} field={field} />
-      ) : (
-        <ReactJson
-          src={isValidJson(field.value) ? field.value : {}}
-          onEdit={handleEdit}
-          onAdd={handleEdit}
-          onDelete={handleEdit}
-          theme={{
-            base00: "rgba(0, 0, 0, 0)",
-            base01: theme.palette.background.default,
-            base02: theme.palette.divider,
-            base03: "#93a1a1",
-            base04: theme.palette.text.disabled,
-            base05: theme.palette.text.secondary,
-            base06: "#073642",
-            base07: theme.palette.text.primary,
-            base08: "#d33682",
-            base09: "#cb4b16",
-            base0A: "#dc322f",
-            base0B: "#859900",
-            base0C: "#6c71c4",
-            base0D: theme.palette.text.secondary,
-            base0E: "#2aa198",
-            base0F: "#268bd2",
-          }}
-          iconStyle="triangle"
-          style={{
-            fontFamily:
-              "SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace",
-            backgroundColor: "transparent",
-          }}
-        />
-      )}
+      {(() => {
+        switch (field.name) {
+          case "holdings":
+            return <HoldingsEditor form={form} field={field} />;
+          case "punchCard":
+            return <PunchCardEditor form={form} field={field} />;
+          default:
+            return (
+              <ReactJson
+                src={isValidJson(field.value) ? field.value : {}}
+                onEdit={handleEdit}
+                onAdd={handleEdit}
+                onDelete={handleEdit}
+                theme={{
+                  base00: "rgba(0, 0, 0, 0)",
+                  base01: theme.palette.background.default,
+                  base02: theme.palette.divider,
+                  base03: "#93a1a1",
+                  base04: theme.palette.text.disabled,
+                  base05: theme.palette.text.secondary,
+                  base06: "#073642",
+                  base07: theme.palette.text.primary,
+                  base08: "#d33682",
+                  base09: "#cb4b16",
+                  base0A: "#dc322f",
+                  base0B: "#859900",
+                  base0C: "#6c71c4",
+                  base0D: theme.palette.text.secondary,
+                  base0E: "#2aa198",
+                  base0F: "#268bd2",
+                }}
+                iconStyle="triangle"
+                style={{
+                  fontFamily:
+                    "SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace",
+                  backgroundColor: "transparent",
+                }}
+              />
+            );
+        }
+      })()}
     </div>
   );
 }
